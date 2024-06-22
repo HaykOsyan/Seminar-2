3b372ff0d8b32c297233c73275b2920799f7f3e2
«HaykOsyan <ohayk@mail.ru>
2024-06-22 11:33:38 +0400

    task 1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿// Задача 1: Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+Console.WriteLine("Enter Number please");
+int number = Convert.ToInt32(Console.ReadLine());
+
+static void CheckIfDivide(int a, int b, int number)
+{
+    if (number % a == 0 && number % b == 0)
+    {
+        System.Console.WriteLine("div");
+    }
+    else
+    {
+        System.Console.WriteLine("no div");
+    }
+}
+CheckIfDivide(7, 23, number);
